8c03f5c0e87278589431be1540c68747b815fdbf
root <root@95-154-196-34.cprapid.com>
2025-01-20 10:32:33 +0000

    home page image upload

--- gitpush.sh
+++ gitpush.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+
+PROJECT_PATH="/home/salonapp/public_html/assets/img/homepage"
+
+# Change directory to the project path
+cd "$PROJECT_PATH" || { echo "Failed to change directory"; exit 1; }
+
 git add .
 git branch -M main
 git commit -m "home page image upload"
